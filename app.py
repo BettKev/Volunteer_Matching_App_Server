7f4c78f35bb49389b2f248b005ad97ff215d951c
Kevin Bett <prodigy4614@gmail.com>
2025-01-27 14:33:12 +0300

    update database initialization

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 from models.user import db  # Import db from user.py
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -12,27 +13,17 @@ CORS(app)
 # Configure the app's secret key and database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # SQLite database (for development)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
-app.config['JWT_SECRET_KEY'] = 'secret_key'  # Optional JWT secret key (if using JWT)
+app.config['JWT_SECRET_KEY'] = 'secret_key'  # Optional JWT secret key
 
-# Initialize the db with the app
+# Initialize the db and Flask-Migrate with the app
 db.init_app(app)
-
-def create_db():
-    """Function to create the database."""
-    if not os.path.exists('app.db'):
-        print("Database does not exist, creating...")
-        db.create_all()
-    else:
-        print("Database already exists.")
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
     return {"message": "Welcome to Dexters Lab API!"}, 200
 
 if __name__ == "__main__":
-    with app.app_context():
-        create_db()  # Create the database and tables if they don't already exist
-
     # Get the port from environment variable or use 5000 by default
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
